5c014e59265aa03918349317c680bb9be2724777
allisonkadel <akadel01@gmail.com>
2020-08-12 16:11:08 -0400

    office hours

---
+++ office-hours/monday.rb
@@ -0,0 +1,13 @@
+# NOTES FROM OFFICE HOURS - MONDAY
+
+# QUESTIONS & ANSWERS
+
+# 1. I see a study group on the Cohort Calendar for Ruby office hours, but not showing up on my Study Group dashboard on learn.co. Why is there a difference?
+    # Learn.co will usually have all study groups and weekly lessons, but the cohort calendar is the best place to go to see the source of truth for schedules!
+# 2. When do we start building the first project?
+
+2. some admin question I forget
+3. Regex lab - 5 words long method
+4. Regex lab - validating phone numbers - using the OR expression
+https://bneijt.nl/pr/ruby-regular-expressions/
+
